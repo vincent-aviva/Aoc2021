7e28b5bcbc7b42714acde05c80ad988ec7c9831b
Vincent van der Laan <vincent.van.der.laan@avivasolutions.nl>
2021-12-03 10:14:45 +0100

    Day 3 part 1

---
+++ Aoc2021.Tests/Day03Tests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Aoc2020.Tests
+{
+    public class Day03Tests
+    {
+        private readonly IDay _day;
+        private readonly List<string> _input;
+
+        public Day03Tests()
+        {
+            _input = new List<string>
+            {
+                "00100",
+                "11110",
+                "10110",
+                "10111",
+                "10101",
+                "01111",
+                "00111",
+                "11100",
+                "10000",
+                "11001",
+                "00010",
+                "01010"
+            };
+
+            _day = new Day03();
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            _day.Solve1(_input).Should().Be(198);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            
+        }
+    }
+}
--- Aoc2021/Day03.cs
+++ Aoc2021/Day03.cs
@@ -6,13 +6,36 @@ namespace Aoc2020
 {
     public class Day03 : IDay
     {
-        public bool IsImplemented => false;
-        public bool IsPart1Complete => false;
+        public bool IsImplemented => true;
+        public bool IsPart1Complete => true;
         public bool IsPart2Complete => false;
 
         public long Solve1(IList<string> input)
         {
-            throw new NotImplementedException();
+            var gammaArray = "";
+            var epsilonArray = "";
+            var rowCount = input.Count;
+            var charCount = input.First().Length;
+
+            for(var i = 0; i < charCount; i++)
+            {
+                var sum = input.Select(x => Convert.ToInt16(x.Substring(i, 1))).Sum(x => x);
+                if(sum > rowCount/2)
+                {
+                    gammaArray += "1";
+                    epsilonArray += "0";
+                }
+                else
+                {
+                    gammaArray += "0";
+                    epsilonArray += "1";
+                }
+            }
+
+            var gamma = Convert.ToInt32(gammaArray, 2);
+            var epsilon = Convert.ToInt32(epsilonArray, 2);
+
+            return gamma * epsilon;
         }
 
         public long Solve2(IList<string> input)
