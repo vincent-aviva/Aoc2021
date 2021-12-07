b0ddbc306635863e24a5ad1eedd3af38988b7356
Vincent van der Laan <vincent.van.der.laan@avivasolutions.nl>
2021-12-07 17:51:45 +0100

    Day 7

---
+++ Aoc2021.Tests/Day07Tests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Aoc2020.Tests
+{
+    public class Day07Tests
+    {
+        private readonly IDay _day;
+        private readonly List<string> _input;
+
+        public Day07Tests()
+        {
+            _input = new List<string>
+            {
+                "16,1,2,0,4,2,7,1,2,14"
+            };
+            _day = new Day07();
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            _day.Solve1(_input).Should().Be(37);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            _day.Solve2(_input).Should().Be(168);
+        }
+    }
+}
--- Aoc2021/Day07.cs
+++ Aoc2021/Day07.cs
@@ -6,18 +6,44 @@ namespace Aoc2020
 {
     public class Day07 : IDay
     {
-        public bool IsImplemented => false;
-        public bool IsPart1Complete => false;
-        public bool IsPart2Complete => false;
+        public bool IsImplemented => true;
+        public bool IsPart1Complete => true;
+        public bool IsPart2Complete => true;
 
         public long Solve1(IList<string> input)
         {
-            throw new NotImplementedException();
+            var initialPosition = input.First().Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt32(x)).ToList();
+            
+            initialPosition.Sort();
+
+            var mid = initialPosition.Count / 2;
+            var median = initialPosition.Count % 2 == 0 ? 
+                initialPosition[mid] :
+                ((initialPosition[mid] + initialPosition[mid + 1]) / 2);
+
+            var fuel = initialPosition.Select(x => x > median ? x -median : median - x).Sum();
+
+            return fuel;
         }
 
         public long Solve2(IList<string> input)
         {
-            throw new NotImplementedException();
+            var initialPosition = input.First().Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt32(x)).ToList();
+
+            var avg1 = initialPosition.Average();
+            var rounded = Math.Floor(avg1); // For the unit test this does not work, round is needed
+
+            var fuel = 0;
+            foreach(var item in initialPosition)
+            {
+                var distance = item > rounded ? item - rounded : rounded - item;
+                for(var i = 1; i <= distance; i++)
+                {
+                    fuel += i;
+                }
+            }
+
+            return fuel;
         }
     }
 }
