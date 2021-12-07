7224780172ca25c5dccca8bd025314b5604abdf6
Vincent van der Laan <vincent.van.der.laan@avivasolutions.nl>
2021-12-07 15:48:35 +0100

    Day 6

---
+++ Aoc2021.Tests/Day06Tests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Aoc2020.Tests
+{
+    public class Day06Tests
+    {
+        private readonly IDay _day;
+        private readonly List<string> _input;
+
+        public Day06Tests()
+        {
+            _input = new List<string>
+            {
+                "3,4,3,1,2"
+            };
+            _day = new Day06();
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            _day.Solve1(_input).Should().Be(5934);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            _day.Solve2(_input).Should().Be(26984457539);
+        }
+    }
+}
--- Aoc2021/Day06.cs
+++ Aoc2021/Day06.cs
@@ -6,18 +6,48 @@ namespace Aoc2020
 {
     public class Day06 : IDay
     {
-        public bool IsImplemented => false;
-        public bool IsPart1Complete => false;
-        public bool IsPart2Complete => false;
+        public bool IsImplemented => true;
+        public bool IsPart1Complete => true;
+        public bool IsPart2Complete => true;
 
         public long Solve1(IList<string> input)
         {
-            throw new NotImplementedException();
+            return Calculate(input.First(), 80);
         }
 
         public long Solve2(IList<string> input)
         {
-            throw new NotImplementedException();
+            return Calculate(input.First(), 256);
+        }
+
+        private long Calculate(string input, int daysToCalculate)
+        {
+            var startFishes = input.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt32(x));
+            long day0Count = startFishes.Count(x => x == 0);
+            long day1Count = startFishes.Count(x => x == 1);
+            long day2Count = startFishes.Count(x => x == 2);
+            long day3Count = startFishes.Count(x => x == 3);
+            long day4Count = startFishes.Count(x => x == 4);
+            long day5Count = startFishes.Count(x => x == 5);
+            long day6Count = startFishes.Count(x => x == 6);
+            long day7Count = startFishes.Count(x => x == 7);
+            long day8Count = startFishes.Count(x => x == 8);
+
+            for (var dayCount = 0; dayCount < daysToCalculate; dayCount++)
+            {
+                long newFishes = day0Count;
+                day0Count = day1Count;
+                day1Count = day2Count;
+                day2Count = day3Count;
+                day3Count = day4Count;
+                day4Count = day5Count;
+                day5Count = day6Count;
+                day6Count = day7Count + newFishes;
+                day7Count = day8Count;
+                day8Count = newFishes;
+            }
+
+            return day0Count + day1Count + day2Count + day3Count + day4Count + day5Count + day6Count + day7Count + day8Count;
         }
     }
 }
