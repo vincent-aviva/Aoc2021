425af69b627e06392c8cbf19d634b75643de2d1f
Vincent van der Laan <vincent.van.der.laan@avivasolutions.nl>
2021-12-08 10:31:30 +0100

    Day 8 - part 1

--- Aoc2021/Day08.cs
+++ Aoc2021/Day08.cs
@@ -6,18 +6,32 @@ namespace Aoc2020
 {
     public class Day08 : IDay
     {
-        public bool IsImplemented => false;
-        public bool IsPart1Complete => false;
+        public bool IsImplemented => true;
+        public bool IsPart1Complete => true;
         public bool IsPart2Complete => false;
 
         public long Solve1(IList<string> input)
         {
-            throw new NotImplementedException();
+            var inputLines = input.Select(x => new InputLine(x));
+            return inputLines.Select(x => x.OutputValue.Count(y => y.Length == 2 || y.Length == 3 || y.Length == 4 || y.Length == 7)).Sum();
         }
 
         public long Solve2(IList<string> input)
         {
             throw new NotImplementedException();
+        }
+    }
+
+    public class InputLine
+    {
+        public List<string> SignalPatters { get; set; }
+        public List<string> OutputValue { get; set; }
+
+        public InputLine(string input)
+        {
+            var splitted = input.Split(" | ");
+            SignalPatters = splitted[0].Split(' ').ToList();
+            OutputValue = splitted[1].Split(' ').ToList();
         }
     }
 }
